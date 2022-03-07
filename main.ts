e0f9d759118aad8590e591a1704ad6941baa24c0
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-03-07 23:31:47 +0100

    Aktualizovat main.blocks, main.ts, lightgate.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-SvetelnaBrana.onLightDrop(function () {
+SvetelnaBrana.kdyzPoruseniHladiny(function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
 })
 SvetelnaBrana.spustitKalibraci(50)
